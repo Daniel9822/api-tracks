f217d4d566192ed124a5f6bd33b03e2849dae0f8
Daniel9822 <dhdany64@gmail.com>
2023-03-13 17:28:24 -0400

    add verify role

--- src/routes/storage.js
+++ src/routes/storage.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const { createItem, getItem, getItems, deleteItem } = require("../controllers/storage");
+const {
+    createItem,
+    getItem,
+    getItems,
+    deleteItem,
+} = require("../controllers/storage");
 const uploadMiddleware = require("../utils/handleUpload");
-const { validateQueryItem } = require('../validators/tracks')
+const { validateQueryItem } = require("../validators/tracks");
 const router = express.Router();
 
-
-router.get('/', getItem)
-router.get('/:id', validateQueryItem, getItems)
-router.post('/',uploadMiddleware.single('myfile'), validateQueryItem, createItem)
-router.delete('/:id', validateQueryItem, deleteItem)
+router.get("/", getItem);
+router.get("/:id", validateQueryItem, getItems);
+router.post(
+    "/",
+    uploadMiddleware.single("myfile"),
+    validateQueryItem,
+    createItem
+);
+router.delete("/:id", validateQueryItem, deleteItem);
 
 module.exports = router;
